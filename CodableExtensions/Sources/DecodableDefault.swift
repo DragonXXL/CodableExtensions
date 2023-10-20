026deff233fc83a6d71a00179100665a5965234a
Dragon <dragon.xuxiaolong@icloud.com>
2023-10-20 19:02:35 +0800

    add exts

---
+++ CodableExtensions/Sources/DecodableDefault.swift
@@ -0,0 +1,286 @@
+
+import Foundation
+
+
+// MARK: - Codable仅支持默认值
+// 例: @Default.EmptyInt var foo: Int?
+enum Default {}
+
+extension Default.Wrapper: Equatable where Value: Equatable {}
+
+
+extension Default.Wrapper: Hashable where Value: Hashable {}
+
+extension Default.Wrapper: Encodable where Value: Encodable {
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(wrappedValue)
+    }
+}
+
+protocol DecodableDefaultSource {
+    associatedtype Value: Decodable
+    static var defaultValue: Value { get }
+}
+
+
+extension Default.Wrapper: Decodable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        wrappedValue = try container.decode(Value.self)
+    }
+}
+
+
+extension KeyedDecodingContainer {
+    func decode<T>(_ type: Default.Wrapper<T>.Type,
+                   forKey key: Key) throws -> Default.Wrapper<T> {
+        try decodeIfPresent(type, forKey: key) ?? .init()
+    }
+}
+
+
+extension Default {
+    @propertyWrapper
+    struct Wrapper<Source: DecodableDefaultSource> {
+        typealias Value = Source.Value
+        var wrappedValue = Source.defaultValue
+    }
+}
+
+
+extension Default {
+
+
+    typealias EmptyList<T: List> = Wrapper<Sources.EmptyList<T>>
+    typealias EmptyMap<T: Map> = Wrapper<Sources.EmptyMap<T>>
+
+    typealias EmptyFloat = Wrapper<Sources.EmptyFloat>
+    typealias EmptyCGFloat = Wrapper<Sources.EmptyCGFloat>
+
+    typealias EmptyString = Wrapper<Sources.EmptyString>
+    typealias EmptyInt = Wrapper<Sources.EmptyInt>
+
+    typealias EmptyIntOne = Wrapper<Sources.EmptyIntOne>
+
+    typealias EmptyDouble = Wrapper<Sources.EmptyDouble>
+
+    typealias EmptyInt64 = Wrapper<Sources.EmptyInt64>
+
+    typealias True = Wrapper<Sources.True>
+    typealias False = Wrapper<Sources.False>
+
+    typealias Source = DecodableDefaultSource
+
+    typealias Map = Decodable & ExpressibleByDictionaryLiteral
+
+    typealias List = Decodable & ExpressibleByArrayLiteral
+
+    enum Sources {
+
+        enum EmptyList<T: List>: Source {
+            static var defaultValue: T { [] }
+        }
+
+        enum EmptyMap<T: Map>: Source {
+            static var defaultValue: T { [:] }
+        }
+
+
+        enum EmptyInt: Source {
+            static var defaultValue: Int { 0 }
+        }
+
+        enum EmptyInt64: Source {
+            static var defaultValue: Int64 { 0 }
+        }
+
+        enum EmptyIntOne: Source {
+            static var defaultValue: Int { 1 }
+        }
+
+        enum EmptyFloat: Source {
+            static var defaultValue: Float { 0.0 }
+        }
+
+        enum EmptyDouble: Source {
+            static var defaultValue: Double { 0.00000 }
+        }
+
+        enum EmptyCGFloat: Source {
+            static var defaultValue: CGFloat { 0.0 }
+        }
+
+        enum EmptyString: Source {
+            static var defaultValue: String { "" }
+        }
+
+
+        enum True: Source {
+            static var defaultValue: Bool { true }
+        }
+
+        enum False: Source {
+            static var defaultValue: Bool { false }
+        }
+
+
+    }
+}
+
+// MARK: - Codable同时支持默认值和Combine得 @Published属性数据流
+// 例: @DefaultPublished.EmptyInt var foo: Int?
+enum DefaultPublished {
+}
+
+protocol PublishedCodableDefaultSource {
+    associatedtype Value: Codable
+
+    static var defaultValue: Value { get }
+}
+
+extension DefaultPublished {
+    @propertyWrapper
+    class Wrapper<Source: PublishedCodableDefaultSource>: Codable {
+        typealias Value = Source.Value
+        @Published var wrappedValue = Source.defaultValue
+
+        var projectedValue: Wrapper {
+            return self
+        }
+
+        required init(from decoder: Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            wrappedValue = try container.decode(Value.self)
+        }
+
+        init() {
+
+        }
+
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.singleValueContainer()
+            try container.encode(wrappedValue)
+        }
+    }
+}
+
+extension DefaultPublished {
+    typealias EmptyList<T: List> = Wrapper<Sources.EmptyList<T>>
+    typealias EmptyMap<T: Map> = Wrapper<Sources.EmptyMap<T>>
+
+
+    typealias EmptyFloat = Wrapper<Sources.EmptyFloat>
+    typealias EmptyCGFloat = Wrapper<Sources.EmptyCGFloat>
+
+    typealias EmptyString = Wrapper<Sources.EmptyString>
+    typealias EmptyInt = Wrapper<Sources.EmptyInt>
+
+    typealias EmptyIntOne = Wrapper<Sources.EmptyIntOne>
+
+    typealias EmptyDouble = Wrapper<Sources.EmptyDouble>
+
+    typealias EmptyInt64 = Wrapper<Sources.EmptyInt64>
+
+
+    typealias True = Wrapper<Sources.True>
+    typealias False = Wrapper<Sources.False>
+
+
+    typealias Map = Codable & ExpressibleByDictionaryLiteral
+
+    typealias List = Codable & ExpressibleByArrayLiteral
+
+    typealias Source = PublishedCodableDefaultSource
+
+    enum Sources {
+
+        enum EmptyList<T: List>: Source {
+            static var defaultValue: T { [] }
+        }
+
+        enum EmptyMap<T: Map>: Source {
+            static var defaultValue: T { [:] }
+        }
+
+
+        enum EmptyInt: Source {
+            static var defaultValue: Int { 0 }
+        }
+
+
+
+        enum EmptyInt64: Source {
+            static var defaultValue: Int64 { 0 }
+        }
+
+        enum EmptyIntOne: Source {
+            static var defaultValue: Int { 1 }
+        }
+
+        enum EmptyFloat: Source {
+            static var defaultValue: Float { 0.0 }
+        }
+
+
+        enum EmptyDouble: Source {
+            static var defaultValue: Double { 0.00000 }
+        }
+
+        enum EmptyCGFloat: Source {
+            static var defaultValue: CGFloat { 0.0 }
+        }
+
+
+        enum EmptyString: Source {
+            static var defaultValue: String { "" }
+        }
+
+
+        enum True: Source {
+            static var defaultValue: Bool { true }
+        }
+
+        enum False: Source {
+            static var defaultValue: Bool { false }
+        }
+
+    }
+
+}
+
+extension KeyedDecodingContainer {
+    func decode<T>(_ type: DefaultPublished.Wrapper<T>.Type,
+                   forKey key: Key) throws -> DefaultPublished.Wrapper<T> {
+        try decodeIfPresent(type, forKey: key) ?? .init()
+    }
+}
+
+
+private class PublishedWrapper<T> {
+    @Published private(set) var value: T
+
+    init(_ value: Published<T>) {
+        _value = value
+    }
+}
+
+extension Published {
+    var unofficialValue: Value {
+        PublishedWrapper(self).value
+    }
+}
+
+extension Published: Decodable where Value: Decodable {
+    public init(from decoder: Decoder) throws {
+        self.init(wrappedValue: try Value.init(from: decoder))
+    }
+}
+
+extension Published: Encodable where Value: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        try unofficialValue.encode(to: encoder)
+    }
+}
+
+
---
+++ CodableExtensions/Sources/ResponseUtils.swift
@@ -0,0 +1,201 @@
+
+
+import Foundation
+
+extension Decodable {
+
+    var data: Data? {
+        return try? JSONSerialization.data(withJSONObject: self, options: [])
+    }
+
+    var dictionary: [String: Any]? {
+        var dictionary = [String: Any]()
+        let mirror = Mirror(reflecting: self)
+        for child in mirror.children {
+            let name = child.label!
+            dictionary[name] = child.value
+        }
+        if dictionary.keys.count > 0 {
+            return dictionary
+        }
+        return nil
+    }
+    
+}
+
+
+extension Array {
+
+    func model<T: Decodable>(_ type: T.Type) -> T? {
+        if let jsonData = data {
+            return jsonData.model(T.self)
+        }
+        debugPrint("Array解析失败\(self)")
+        return nil
+    }
+
+    func toJSONString() -> String? {
+        if !JSONSerialization.isValidJSONObject(self) {
+            debugPrint("Array转json失败")
+            return nil
+        }
+        if let jsonData = data {
+            let JSONString = String(data: jsonData, encoding: .utf8)
+            return JSONString
+        }
+        debugPrint("Arrayt转json失败")
+        return nil
+    }
+
+    var data: Data? {
+        return try? JSONSerialization.data(withJSONObject: self, options: [])
+    }
+}
+
+
+extension String {
+
+    func toDict() -> [String: Any]? {
+        guard let jsonData: Data = data(using: .utf8) else {
+            debugPrint("jsonString转dict失败")
+            return nil
+        }
+        if let dict = try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers) {
+            return dict as? [String: Any] ?? ["": ""]
+        }
+        debugPrint("jsonString转dict失败")
+        return nil
+    }
+
+    func toArray() -> [Any]? {
+        guard let jsonData: Data = data(using: .utf8) else {
+            debugPrint("jsonString转Array失败")
+            return nil
+        }
+        if let array = try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers) {
+            return array as? [Any] ?? [""]
+        }
+        debugPrint("jsonString转Array失败")
+        return nil
+    }
+
+    /// json字符串转模型
+    func model<T: Decodable>(_ type: T.Type) -> T? {
+        if let jsonData = data(using: .utf8) {
+            return jsonData.model(type)
+        }
+        debugPrint("解析失败\(self)")
+        return nil
+    }
+
+}
+
+
+extension Data {
+
+    func utf8String() -> String? {
+        if let utf8 = String(data: self, encoding: .utf8) {
+            return utf8
+        }
+        return nil
+    }
+
+    var toDict: [String: Any]? {
+        if !JSONSerialization.isValidJSONObject(self) {
+            debugPrint("jsonData转dict失败")
+            return nil
+        }
+        if let dict = try? JSONSerialization.jsonObject(with: self, options: .mutableContainers) {
+            return dict as? [String: Any] ?? ["": ""]
+        }
+        debugPrint("jsonData转dict失败")
+        return nil
+    }
+
+    var toArray: [Any]? {
+        if !JSONSerialization.isValidJSONObject(self) {
+            debugPrint("jsonData转Array失败")
+            return nil
+        }
+
+        if let array = try? JSONSerialization.jsonObject(with: self, options: .mutableContainers) {
+            return array as? [Any] ?? [""]
+        }
+        debugPrint("jsonData转Array失败")
+        return nil
+    }
+
+    /// json data 转模型
+    /// 可以直接拿网络请求成功的response直接转换
+    func model<T: Decodable>(_ type: T.Type) -> T? {
+        do {
+            return try JSONDecoder().decode(type, from: self)
+        } catch let err {
+            debugPrint("[jsonData] 解析失败\(String(describing: self.utf8String())) ---- error: \(err)")
+            return nil
+        }
+    }
+
+}
+
+
+extension Dictionary {
+
+    func toJSONString() -> String? {
+        if !JSONSerialization.isValidJSONObject(self) {
+            debugPrint("dict转json失败\(self)")
+            return nil
+        }
+        if let jsonData = data {
+            let JSONString = String(data: jsonData, encoding: .utf8)
+            return JSONString
+        }
+        debugPrint("dict转json失败\(self)")
+        return nil
+    }
+
+    var data: Data? {
+        return try? JSONSerialization.data(withJSONObject: self, options: [])
+    }
+
+    func model<T: Decodable>(_ type: T.Type) -> T? {
+        if let jsonData = data {
+            return jsonData.model(T.self)
+        }
+        debugPrint("dict解析失败\(self)")
+        return nil
+    }
+
+}
+
+
+struct ResponseModel<T>: Codable where T: Codable {
+
+    var error: String?
+
+    var data: T?
+
+    /// 0以上为正常, -20为未知错误，-10表示有错误详情
+    var code: Int = -10
+
+    var headerToken: String?
+
+    var message: [String]?
+
+    var msg: String? {
+        if code != 0 {
+//            return LaunchCore.textFori18nConfig(code: "\(code)", messages: message)
+        }
+        return nil
+    }
+
+}
+
+struct ResponseData: Codable {}
+
+class ResponsePageData<T: Codable>: Codable {
+    var arrays: [T]?
+    @Default.EmptyIntOne var current = 1
+    @Default.EmptyInt var total = 0
+    @Default.EmptyInt var totalPage = 0
+}
